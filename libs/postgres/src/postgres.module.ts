1a411d452fe101ee6bf172af11db6b7d2e7334be
Retr0327 <retr0327.dev@gmail.com>
2024-07-19 22:27:59 +0800

    feat: implement postgresql module (ref #1)

---
+++ libs/postgres/src/postgres.module.ts
@@ -0,0 +1,24 @@
+import { Module, Global } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import env from '@sol/libs/env';
+import { typeOrmModuleOptions } from './config';
+import { entities } from './entities';
+import { repositories } from './repositories';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [env],
+      cache: true,
+    }),
+
+    TypeOrmModule.forRoot({ ...typeOrmModuleOptions, entities }),
+    TypeOrmModule.forFeature(entities),
+  ],
+  providers: [...repositories],
+  exports: [TypeOrmModule, ...repositories],
+})
+@Global()
+export class PostgresModule {}
