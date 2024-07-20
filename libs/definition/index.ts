8b7f42c0d2074d4848f7b5472b77a01bf7d2d65b
Retr0327 <retr0327.dev@gmail.com>
2024-07-21 01:39:46 +0800

    feat: define namespace and enums (ref #1)

---
+++ libs/definition/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+namespace Definition {
+  export enum Cookies {
+    CsrfToken = 'lcsrl',
+    UserAuthToken = 'lope-auth-tk',
+    UserAuthTokenSig = 'lope-auth-tk.sig',
+  }
+}
+
+export default Definition;
