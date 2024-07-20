78464586d7577884c2642ab1c148a508c0eed41e
Retr0327 <retr0327.dev@gmail.com>
2024-07-21 02:06:41 +0800

    feat: implement user refresh token update domain event interface (ref #1)

---
+++ apps/corpus/src/domain/events/user-refreshToken.event.ts
@@ -0,0 +1,17 @@
+import { DomainEvent } from "@sol/domain/event";
+
+export class UpdateUserRefreshTokenEvent implements DomainEvent {
+  readonly occurredAt: Date;
+  readonly name: string;
+  readonly version: number;
+  readonly id: string;
+  readonly refreshToken: string;
+
+  constructor(props: { id: string; refreshToken: string }) {
+    this.name = 'UserRefreshTokenUpdated';
+    this.occurredAt = new Date();
+    this.version = 1;
+    this.id = props.id;
+    this.refreshToken = props.refreshToken;
+  }
+}
