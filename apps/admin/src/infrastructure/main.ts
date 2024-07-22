6ced8cfc8257ea75369f09453e8e22f7322c92ee
Retr0327 <retr0327.dev@gmail.com>
2024-07-22 20:46:34 +0800

    feat: enhance bootstrap process (ref #2)

--- apps/admin/src/infrastructure/main.ts
+++ apps/admin/src/infrastructure/main.ts
@@ -1,8 +1,26 @@
+import '../application/types/express.d.ts';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { env } from '@sol/env';
+import { HttpExceptionFilter } from '@sol/errors/filters';
 import { AdminModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AdminModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api/v1');
+  app.use(cookieParser(env.cookieSecret));
+  app.use(helmet());
   await app.listen(3000);
 }
+
 bootstrap();
