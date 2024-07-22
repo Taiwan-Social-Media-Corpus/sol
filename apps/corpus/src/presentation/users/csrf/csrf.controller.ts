c09bc935b26da81ccda35fe89dd5ebc029152e56
Retr0327 <retr0327.dev@gmail.com>
2024-07-22 20:18:33 +0800

    feat: implement user CSRF controller (ref #1)

---
+++ apps/corpus/src/presentation/users/csrf/csrf.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { CsrfTokenService, CookiesService } from '@sol/cookies';
+import Definition from '@sol/definition';
+
+@Controller('user/_ping')
+class UserCsrfController {
+  constructor(
+    private readonly csrfTokenService: CsrfTokenService,
+    private readonly cookiesService: CookiesService,
+  ) {}
+  @Get()
+  async signUp(@Req() req: Request, @Res() res: Response) {
+    const csrfToken = this.csrfTokenService.generate();
+    const hmac = this.csrfTokenService.hmac(csrfToken);
+    res.header('X-Csrf-Token', hmac);
+
+    const cookies = req.context.getCookies();
+    this.cookiesService.set(cookies, Definition.Cookies.CsrfToken, csrfToken, {
+      expires: undefined,
+      sameSite: undefined,
+    });
+    return res.status(204).json();
+  }
+}
+
+export default UserCsrfController;
