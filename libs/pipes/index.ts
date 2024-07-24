336f506e34a75ccfd4f9f57e7728f4b55611ff5e
Retr0327 <retr0327.dev@gmail.com>
2024-07-25 01:20:02 +0800

    refactor: remove specific error handling (ref #2)

--- libs/pipes/index.ts
+++ libs/pipes/index.ts
@@ -2,9 +2,8 @@ import {
   PipeTransform,
   ArgumentMetadata,
   UnprocessableEntityException,
-  BadRequestException,
 } from '@nestjs/common';
-import { ZodSchema, ZodError } from 'zod';
+import { ZodSchema } from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
@@ -15,13 +14,6 @@ export class ZodValidationPipe implements PipeTransform {
       const parsedValue = this.schema.parse(value);
       return parsedValue;
     } catch (error) {
-      if (error instanceof ZodError) {
-        const { issues } = error;
-        if (issues.length) {
-          const { message } = issues[0];
-          throw new BadRequestException(message);
-        }
-      }
       throw new UnprocessableEntityException();
     }
   }
