4ec9b1c8a5ac10a34d78612c58772ec92087d906
Retr0327 <retr0327.dev@gmail.com>
2024-07-25 01:16:07 +0800

    feat: implement admin login query (ref #2)

---
+++ apps/admin/src/application/use-cases/admin/queries/login/query.ts
@@ -0,0 +1,9 @@
+export class AdminLoginQuery {
+  readonly email: string;
+  readonly password: string;
+
+  constructor(props: { email: string; password: string }) {
+    this.email = props.email;
+    this.password = props.password;
+  }
+}
