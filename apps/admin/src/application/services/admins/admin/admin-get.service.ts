e24e2bdc0e504f588c5f87cee23cb880fb6afe56
Retr0327 <retr0327.dev@gmail.com>
2024-07-25 01:09:32 +0800

    feat: apply event publisher for context merging (ref #2)

--- apps/admin/src/application/services/admins/admin/admin-get.service.ts
+++ apps/admin/src/application/services/admins/admin/admin-get.service.ts
@@ -4,6 +4,7 @@ import {
   NotFoundException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import { EventPublisher } from '@nestjs/cqrs';
 import { AdminAggregate } from '@sol/admin/domain/models/aggregate-root/admin';
 import { AdminRepository } from '@sol/admin/infrastructure/repositories';
 import { ApplicationService } from '@sol/domain/service';
@@ -20,14 +21,17 @@ export type GetAdminOutput = Promise<Ok<AdminAggregate> | Err<HttpException>>;
 class GetAdminService
   implements ApplicationService<GetAdminInput, GetAdminOutput>
 {
-  constructor(private readonly adminRepository: AdminRepository) {}
+  constructor(
+    private readonly adminRepository: AdminRepository,
+    private readonly eventPublisher: EventPublisher,
+  ) {}
   async execute(input: GetAdminInput) {
     try {
       const adminEntity = await this.adminRepository.findOne(input);
       if (!adminEntity) {
         return Result.Err(new NotFoundException('Admin not found'));
       }
-      return Result.Ok(adminEntity);
+      return Result.Ok(this.eventPublisher.mergeObjectContext(adminEntity));
     } catch (error) {
       return Result.Err(new InternalServerErrorException());
     }
