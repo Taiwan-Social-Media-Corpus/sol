19fee0ba1d138828787811ce89ade0022c008fb0
Retr0327 <retr0327.dev@gmail.com>
2024-07-30 01:34:30 +0800

    feat: export corpus-related queries (ref #3)

---
+++ apps/corpus/src/application/use-cases/corpus/queries/index.ts
@@ -0,0 +1,11 @@
+import { GetCorpusConcordanceQuery } from './concordance/query';
+import { GetCorpusBoardsQuery } from './metadata/boards/query';
+import { GetCorpusInfoQuery } from './metadata/corpus/query';
+import { GetCorpusMediaQuery } from './metadata/media/query';
+
+export {
+  GetCorpusConcordanceQuery,
+  GetCorpusBoardsQuery,
+  GetCorpusInfoQuery,
+  GetCorpusMediaQuery,
+};
