bd7d761c1120e5fc021e6e9b5d3552cad3eafb81
Retr0327 <retr0327.dev@gmail.com>
2024-07-30 01:42:04 +0800

    feat: export corpus-related controllers (ref #3)

--- apps/corpus/src/presentation/index.ts
+++ apps/corpus/src/presentation/index.ts
@@ -1,13 +1,25 @@
+import CorpusConcordanceController from './corpus/concordance/concordance.controller';
+import CorpusMetadataController from './corpus/metadata/metadata.controller';
 import UserAuthController from './users/auth/auth.controller';
 import UserCsrfController from './users/csrf/csrf.controller';
 import UserRefreshController from './users/refresh/refresh.controller';
 import UserSessionsController from './users/sessions/sessions.controller';
 
 const controllers = [
+  CorpusConcordanceController,
+  CorpusMetadataController,
   UserAuthController,
   UserCsrfController,
   UserRefreshController,
   UserSessionsController,
 ];
 
-export { controllers, UserAuthController };
+export {
+  controllers,
+  CorpusConcordanceController,
+  CorpusMetadataController,
+  UserAuthController,
+  UserCsrfController,
+  UserRefreshController,
+  UserSessionsController,
+};
