ff5e56dfc2431e2ed511a22f3bd91e63a752b189
Retr0327 <retr0327.dev@gmail.com>
2024-07-30 01:36:56 +0800

    feat: export corpus-related query handlers (ref #3)

--- apps/corpus/src/application/use-cases/index.ts
+++ apps/corpus/src/application/use-cases/index.ts
@@ -1,10 +1,17 @@
+import { GetCorpusConcordanceQueryHandler } from './corpus/queries/concordance/handler';
+import { GetCorpusBoardsQueryHandler } from './corpus/queries/metadata/boards/handler';
+import { GetCorpusInfoQueryHandler } from './corpus/queries/metadata/corpus/handler';
+import { GetCorpusMediaQueryHandler } from './corpus/queries/metadata/media/handler';
 import { CreateUserCommandHandler } from './user/commands/create/handler';
 import { CheckUserRefreshTokenQueryHandler } from './user/queries/refreshToken/handler';
 
 const cqrsHandlers = [
+  GetCorpusConcordanceQueryHandler,
+  GetCorpusBoardsQueryHandler,
+  GetCorpusInfoQueryHandler,
+  GetCorpusMediaQueryHandler,
   CreateUserCommandHandler,
   CheckUserRefreshTokenQueryHandler,
 ] as const;
 
 export { cqrsHandlers };
- 
